737a2bb966263712c8a09e38b7b14802d4220de9
mrcondor <mr_condor_@hotmail.com>
2018-05-26 22:51:43 -0300

    Desafio de background com imagens dinamicas

--- desafioImg/beluga.js
+++ desafioImg/beluga.js
@@ -1,21 +1,21 @@
-function calcularMedia() {
+function calcularMedia(){                    
+                    
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
-
-    while (typeof arguments[x] === 'number') {
+    //while(arguments[x]){
+    while(typeof arguments[x] === 'number'){
         total += arguments[x];
         x++
-    }
+    }                    
 
     return (total / qtd).toFixed(2);
 }
 
-function sortear(n) {
+function sortear(n){
     var _n = n || 1;
     var nSorteado = Math.random();
     nSorteado = nSorteado * _n;
     nSorteado = Math.floor(nSorteado);
-
     return nSorteado;
 }
