260e0d5ea47bbc446f79c25a24f77a6b7c8b13fb
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2018-05-28 23:39:43 -0300

    Prototypes e melhorias no framework beluga.js

--- beluga2.js
+++ beluga2.js
@@ -4,5 +4,24 @@ var blg = (function () {
     beluga.$ = document.querySelector.bind(document);
     beluga.$S = document.querySelectorAll.bind(document);
 
+    Array.prototype.getMedia = function () {
+        // return _calcularMedia.apply(this, this);
+        return _calcularMedia(...this);
+    };
+
+    function _calcularMedia() {
+        let params = arguments;
+        let hasNaN = Array.prototype.some.call(params, (n) => isNaN(n));
+
+        if (hasNaN) throw new Error('Somente números');
+
+        let total = Array.prototype.reduce.call(params, function (total, atual) {
+            return total + parseFloat(atual);
+        });
+        return total / arguments.length;
+    }
+
+    beluga.calcularMedia = _calcularMedia;
+
     return beluga;
 })()
